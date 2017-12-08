105b45143b36e480b980257ca1cf63c92e3a649c
Kyle <khayes888@gmail.com>
2017-12-08 10:13:14 -0600

    Added code to cache inverse of matrix and call to the cached value if it exists

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall this script does 2 things: 1.) it defines a function that creates an object that caches the 
+## inverse of a matrix 2.) next it defines a function that either gets the cached inverse if it exists
+## or calculates the inverse in the case of non-existence 
 
-## Write a short comment describing this function
+## This function creates a special matrix object that can cache the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    matrix1 <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the
+## matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+  
         ## Return a matrix that is the inverse of 'x'
 }
+
+#The following code demonstrates that the two functions perform as expected
+#Create a test matrix that I know is invertibel
+matrix1 <- matrix(c(1,1,1.5,-1),2,2)
+solve(matrix1)
+
+#Define Cached object
+cacheobject <- makeCacheMatrix(matrix1)
+cacheSolve(cacheobject)
